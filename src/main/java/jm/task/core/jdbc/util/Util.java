7302c59845b02bcd0a161524fb3da824cda46907
@MariaWhitch <dontworrybehappymary@gmail.com>
2022-06-04 21:55:23 +0400

    version

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,10 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class Util {
     // реализуйте настройку соеденения с БД
@@ -14,17 +14,28 @@ public class Util {
     private static final String LOGIN = "root";
     private static final String PASSWORD = "969Pqb13fwz12!900m";
 
-    private static Connection connection;
     private static SessionFactory sessionFactory;
 
-    public static Connection getConnection() {
-        if (connection == null) {
-            try {
-                connection = DriverManager.getConnection(HOST, LOGIN, PASSWORD);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+    public static SessionFactory getConnection() {
+        try {
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.driver_class", DRIVER)
+                    .setProperty("hibernate.connection.url", HOST)
+                    .setProperty("hibernate.connection.username", LOGIN)
+                    .setProperty("hibernate.connection.password", PASSWORD)
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .addAnnotatedClass(User.class)
+                    .setProperty("hibernate.c3p0.min_size", "5")
+                    .setProperty("hibernate.c3p0.max_size", "200")
+                    .setProperty("hibernate.c3p0.max_statements", "200");
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (HibernateException e) {
+            e.printStackTrace();
         }
-        return connection;
+        return sessionFactory;
     }
 }
+
